ed318401de5f7e5046b6d1947b5ae39e2f9eac11
felixfern <felixzhuang1@gmail.com>
2024-04-17 22:03:29 +0700

    feat: small ui styling adjustment

--- src/components/custom/Sidebar/components/FileList.tsx
+++ src/components/custom/Sidebar/components/FileList.tsx
@@ -37,7 +37,7 @@ const FileList = () => {
 								{val.file_name}
 							</TooltipContent>
 							<TooltipTrigger>
-								<div className="relative p-2 border-[1px] dark:border-slate-700 border-slate-400 rounded-md cursor-pointer hover:opacity-60 transition-opacity">
+								<div className="relative p-2 border-[1px] dark:border-slate-700 border-slate-200 rounded-md cursor-pointer hover:opacity-60 transition-opacity">
 									<FileIcon width={14} height={14} />
 									<div className="absolute w-2 h-2 flex items-center justify-center right-[-8px] text-sm bottom-[-8px] p-2 dark:bg-slate-950 bg-white">
 										{index + 1}
